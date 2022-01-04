2fa065ca6453ae6ae5bba2066ebb8da375660666
captain0owais <87132814+captain0owais@users.noreply.github.com>
2022-01-04 11:23:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+let camera=document.getElementById("camera");
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:150
+});
+
+Webcam.attach('#camera');
+function takeSnapShot(){
+    Webcam.snap(function(data_uri){document.getElementById("captured_img").src=data_uri})
+}
+
+console.log("ml5 Version",ml5.version);
+
+let classifier=ml5.imageClassifier('',modelLoded);
+
+function modelLoded(){
+    console.log("MODEL LODED!!");
+}
