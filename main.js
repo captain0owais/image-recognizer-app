7fb4728ec3685dba38e8beab99875698cd591a81
captain0owais <87132814+captain0owais@users.noreply.github.com>
2022-01-08 18:38:12 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -13,8 +13,25 @@ function takeSnapShot(){
 
 console.log("ml5 Version",ml5.version);
 
-let classifier=ml5.imageClassifier('',modelLoded);
+let classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/-4IQlRMiS/model.json',modelLoded)
 
 function modelLoded(){
     console.log("MODEL LODED!!");
+}
+
+function check()
+{
+    img = document.getElementById("captured_img");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+    if (results){
+        console.log(results);
+        document.getElementById("result-object-name").innerHTML=results[0].label;
+        document.getElementById("result-object-accuracy").innerHTML=results[0].confidence.toFixed(3);
+    }else{
+        console.error(error);
+        console.log("error occured");
+    }
 }
